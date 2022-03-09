453d6795e5a9e232fe583b571572df64d104b02d
testingbatch143 <testingbatch143@gmail.com>
2022-03-09 07:59:31 +0530

    app constant changes

--- BDDcucumber/src/test/java/com/bdd/test/AppConstants.java
+++ BDDcucumber/src/test/java/com/bdd/test/AppConstants.java
@@ -3,5 +3,6 @@ package com.bdd.test;
 public class AppConstants {
 
 	public static String browserValue = "Chrome";
-	public static String runMode = "Local"; 
+	public static String runMode = "Local";
+	public static String buildNumber = "100001";
 }
